56c97dc4f68babf4d91b71e5a937d0d5ff1c571f
Batia Szkop <bszkop@amia.org.ar>
2024-06-26 16:00:27 -0300

    trabajando websockets1

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,15 +1,27 @@
 const socket = io();
 
-console.log ("funciona");
+socket.on("connect", () => {
+    console.log("Conectado al servidor");
+});
 
+socket.on("welcome", (message) => {
+    console.log("Mensaje de bienvenida:", message);
+}); 
 socket.on("products", (data) => {
-    renderProducts(data);
-})
+    console.log("Productos recibidos:", data);
+    if (Array.isArray(data)) {
+        renderProducts(data); // Llama a la función para renderizar productos si data es un array
+    } else {
+        console.error("La data recibida no es un array:", data);
+        // Manejo de errores según sea necesario
+    }
+});
 //funcion para renderizar
 const renderProducts = (data) => {
     const contenedorProductos = document.getElementById("contenedorProductos");
+    contenedorProductos.innerHTML = "";
 
-    data.forEach(element => {
+    data.forEach(item => {
         const card = document.createElement("div");
         //card.classList
         card.innerHTML =`<p> ${item.id} </p>
@@ -18,5 +30,42 @@ const renderProducts = (data) => {
                         <button> Eliminar </button>`
                     
         contenedorProductos.appendChild(card);
+        //event for button eliminar
+        card.querySelector("button").addEventListener("click", () => {
+            eliminarProducto(item.id);
+        })
     });
+};
+const eliminarProducto = (id) => {
+    socket.emit("eliminarProducto",id);
+}
+
+//add product from form
+document.getElementById("agregar").addEventListener("click", () => {
+    agregarProducto();
+});
+
+const agregarProducto = () => {
+
+    const titulo = document.getElementById("titulo").value;
+    const descripcion = document.getElementById("descripcion").value;
+    const codigo = document.getElementById("codigo").value;
+    const precio = parseFloat(document.getElementById("precio").value);
+    const stock = parseInt(document.getElementById("stock").value);
+    const categoria = document.getElementById("categoria").value;
+    const status = document.getElementById("status").value === "true";
+
+     const nuevoProducto = {
+        title: titulo,
+        description: descripcion,
+        code: codigo,
+        price: precio,
+        stock: stock,
+        category: categoria,
+    };
+
+    socket.emit("agregarProducto", nuevoProducto);
+
+    document.getElementById("formularioAgregarProducto").reset();
+
 }
