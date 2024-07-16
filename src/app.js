7a019d6f52eda33e0d2d2b299bcfe2d7ef1bdd49
judith braver <bjb64@hotmail.com>
2024-07-16 02:18:06 -0300

    resolviendo merge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import { createServer } from "http";
-import { createServer } from "http";
 import { Server } from "socket.io";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
@@ -19,12 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./src/public"));
 
 // Express-Handlebars
-// Express-Handlebars
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./src/views");
 
-// Rutas
 // Rutas
 app.use("/api/carts", cartsRouter);
 app.use("/api/products", productsRouter);
@@ -65,43 +62,6 @@ io.on("connection", async (socket) => {
 
 // Escuchar el puerto
 httpServer.listen(PUERTO, () => {
-app.use("/", viewsRouter);
-
-// Crea el servidor HTTP usando createServer de http
-const httpServer = createServer(app);
-
-// Inicia Socket.IO y pasar el servidor HTTP
-const io = new Server(httpServer);
-
-// Instancia de ProductManager
-const productManager = new ProductManager("./src/data/products.json");
-
-// Maneja eventos de conexión
-io.on("connection", async (socket) => {
-    console.log("Un cliente se conectó");
-
-    try {
-        const products = await productManager.getProducts();
-        socket.emit("products", products);
-
-        socket.on("eliminarProducto", async (id) => {
-            await productManager.deleteProduct(id);
-            const productosActualizados = await productManager.getProducts();
-            io.emit("products", productosActualizados); 
-        });
-
-        socket.on("agregarProducto", async (producto) => {
-            await productManager.addProduct(producto);
-            const productosActualizados = await productManager.getProducts();
-            io.emit("products", productosActualizados);
-        });
-    } catch (error) {
-        console.error("Error al obtener productos:", error);
-    }
-});
-
-// Escuchar el puerto
-httpServer.listen(PUERTO, () => {
     console.log(`Escuchando en el puerto: ${PUERTO}`);
-});
+    app.use("/", viewsRouter);
 });
